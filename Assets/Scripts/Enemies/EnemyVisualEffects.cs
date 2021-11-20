1ea7e508c60917d52233a810a06ccc9f3e081e2c
Asfand <asbutt@ualberta.ca>
2021-11-20 02:06:15 -0800

    Add Hit effect
    Add enemyVisualEffects script
    Add Vfx graph to package-json and manifest

---
+++ Assets/Scripts/Enemies/EnemyVisualEffects.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class EnemyVisualEffects : MonoBehaviour
+    {
+        [SerializeField] private Enemy _enemy;
+        [SerializeField] private GameObject _hitEffect;
+        
+        private void OnEnable()
+        {
+            Enemy.OnEnemyDied += OnEnemyDied;
+        }
+
+        private void OnDestroy()
+        {
+            Enemy.OnEnemyDied -= OnEnemyDied;
+        }
+        
+        private void OnEnemyDied(Enemy enemy)
+        {
+            if (_enemy != enemy) { return; }
+            
+            Instantiate(_hitEffect, transform.position, Quaternion.identity);
+        }
+    }
+}
